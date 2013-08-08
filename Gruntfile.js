ae428c4a7f5a2b7f740e5a92d249d1ed1335c7de
Bartvds <bartvanderschoor@gmail.com>
2013-08-08 04:29:56 +0200

    added eslint-grunt demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-vows-runner');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('eslint-grunt');
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -28,6 +30,18 @@ module.exports = function (grunt) {
 			pass: {
 				src: ['tests/lib/**/*.js']
 			}
+		},
+		eslint: {
+			demo: {
+				options: {
+					config: 'tests/eslint-demo.json',
+					formatter: 'index.js'
+				},
+				files: {
+					src: ['tests/fixtures/**/*.js']
+				}
+			}
+
 		}
 	});
 
@@ -35,5 +49,8 @@ module.exports = function (grunt) {
 	grunt.registerTask('build', ['jshint:all']);
 
 	grunt.registerTask('test', ['build', 'vows:pass']);
+	grunt.registerTask('demo', ['eslint:demo']);
+
+	grunt.registerTask('dev', ['demo']);
 
 };
